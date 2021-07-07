96be792f86bddb76819d970d402b612f2a01a02f
Jordan Williams <jordan@jwillikers.com>
2021-07-07 18:36:09 -0500

    Don't pass architecture in the test script

--- test.fish
+++ test.fish
@@ -1,21 +1,14 @@
 #!/usr/bin/env fish
 
-set -l options (fish_opt --short a --long architecture --required-val)
-set -a options (fish_opt --short h --long help)
+set -l options (fish_opt --short h --long help)
 
 argparse --max-args 0 $options -- $argv
 or exit
 
 if set -q _flag_help
-    echo "build [-a|--architecture] [-h|--help]"
+    echo "build [-h|--help]"
     exit 0
 end
 
-set -l architecture (podman info --format={{".Host.Arch"}})
-if set -q _flag_architecture
-    set architecture $_flag_architecture
-end
-echo "The image will be tested for the $architecture architecture."
-
-podman run --rm --arch $architecture --name test-container -it localhost/screen -v
+podman run --rm --name test-container -it localhost/screen:latest -v
 or exit
