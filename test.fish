82926fccf32b2337f70e2f87a5372669faa0fa08
Jordan Williams <jordan@jwillikers.com>
2021-07-08 09:08:43 -0500

    Name images according to architecture

--- test.fish
+++ test.fish
@@ -1,14 +1,20 @@
 #!/usr/bin/env fish
 
 set -l options (fish_opt --short h --long help)
+set -a options (fish_opt --short n --long name --required-val)
 
 argparse --max-args 0 $options -- $argv
 or exit
 
 if set -q _flag_help
-    echo "build [-h|--help]"
+    echo "build [-h|--help] [-n|--name]"
     exit 0
 end
 
-podman run --rm --name test-container -it localhost/screen:latest -v
+set -l name screen
+if set -q _flag_name
+    set name $_flag_name
+end
+
+podman run --rm --name test-container -it localhost/$name:latest -v
 or exit
